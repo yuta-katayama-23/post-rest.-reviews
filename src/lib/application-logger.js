258f4a06528454fef5acfa361d051f19c8b0f7c7
Yuta Katayama <yutapilafu@gmail.com>
2022-01-03 12:09:08 +0900

    アプリケーションログでoptionsを設定できるように修正

--- src/lib/application-logger.js
+++ src/lib/application-logger.js
@@ -1,6 +1,7 @@
 import { AppLogger } from './logger';
 
-export default () => {
+// eslint-disable-next-line no-unused-vars
+export default (options = {}) => {
 	return (err, req, res, next) => {
 		AppLogger.error(err.message);
 		next(err);
