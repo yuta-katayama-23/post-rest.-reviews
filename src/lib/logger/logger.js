fc6091b59a33a60822da71d878c66fafc994fb3f
Yuta Katayama <yutapilafu@gmail.com>
2022-01-13 17:40:54 +0900

    exportを変更

--- src/lib/logger/logger.js
+++ src/lib/logger/logger.js
@@ -3,6 +3,8 @@ import config from '../../../config/log4js.config';
 
 log4js.configure(config);
 
-export const DefaultLogger = log4js.getLogger();
-export const AppLogger = log4js.getLogger('application');
-export const AccessLogger = log4js.getLogger('access');
+const DefaultLogger = log4js.getLogger();
+const AppLogger = log4js.getLogger('application');
+const AccessLogger = log4js.getLogger('access');
+
+export { AccessLogger, DefaultLogger, AppLogger };
