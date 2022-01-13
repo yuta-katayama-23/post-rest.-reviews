53a2c9b0001c9ec1ed52b51a9a2b622eb6b09366
Yuta Katayama <yutapilafu@gmail.com>
2022-01-13 21:52:51 +0900

    res.localsにmoment, paddingを設定

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import 'source-map-support/register';
 import express from 'express';
 import appRoot from 'app-root-path';
 import favicon from 'serve-favicon';
+import moment from 'moment';
 import router from './routes/index';
 import { AppLogger } from './lib/logger/logger';
 import applicationLogger from './lib/logger/application-logger';
 import accessLogger from './lib/logger/access-logger';
 import mysqlClient from './lib/database/client';
+import { padding } from './lib/math/math';
 
 const port = process.env.PORT;
 const app = express();
@@ -15,10 +17,15 @@ app.set('view engine', 'ejs');
 app.set('views', appRoot.resolve('src/views'));
 app.disable('x-powered-by');
 
+mysqlClient(app);
+app.use((req, res, next) => {
+	res.locals.moment = moment;
+	res.locals.padding = padding;
+	next();
+});
+
 app.use(favicon(appRoot.resolve('src/public/favicon.ico')));
 app.use('/public', express.static(appRoot.resolve('src/public')));
-
-mysqlClient(app);
 
 app.use(accessLogger());
 
---
+++ src/lib/math/math.js
@@ -0,0 +1,14 @@
+import { roundTo } from 'round-to';
+
+const padding = (value) => {
+	if (Number.isNaN(value)) {
+		return '-';
+	}
+	return roundTo(value, 2).toPrecision(3);
+};
+
+const round = (value) => {
+	return roundTo(value, 2);
+};
+
+export { padding, round };
