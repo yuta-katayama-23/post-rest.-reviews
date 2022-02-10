9d452ef0992ef688422304d3179a35eb71e150a8
Yuta Katayama <yutapilafu@gmail.com>
2022-02-10 09:27:33 +0900

    express-sessionを導入

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 import 'source-map-support/register';
 import express from 'express';
 import * as helmet from 'helmet';
-import cookie from 'cookie-parser';
 import appRoot from 'app-root-path';
 import favicon from 'serve-favicon';
 import moment from 'moment';
+import config from 'config';
+
+import expressSession from 'express-session';
+import connectRedis from 'connect-redis';
+import Redis from 'ioredis';
+
 import router from './routes/index';
 import shopsRouter from './routes/shops';
 import searchRouter from './routes/search';
 import accountRouter from './routes/account';
+
 import { AppLogger } from './lib/logger/logger';
 import applicationLogger from './lib/logger/application-logger';
 import accessLogger from './lib/logger/access-logger';
+
 import mysqlClient from './lib/database/client';
 import { padding } from './lib/math/math';
 
@@ -20,6 +27,18 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', appRoot.resolve('src/views'));
+
+const redis = new Redis();
+const RedisStore = connectRedis(expressSession);
+const store = new RedisStore({ client: redis });
+
+app.use(
+	expressSession({
+		...config.get('redis.session'),
+		secret: process.env.COOKIE_SECRET,
+		store
+	})
+);
 
 app.use(helmet.hidePoweredBy());
 app.use(helmet.xssFilter());
@@ -61,16 +80,21 @@ app.use('/public', express.static(appRoot.resolve('src/public')));
 
 app.use(accessLogger());
 
-app.use(cookie());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
 	const {
-		cookies: { message }
+		session,
+		query: { login }
 	} = req;
 
-	console.log(message);
-	res.cookie('message', 'hello world!', { httpOnly: true });
+	if (session.id && !session.login)
+		console.log('初回 セッション（Cookie未保存）', session.id);
+	if (session.login) console.log('login is ', session.login, session.id);
 
+	if (login) {
+		console.log('login処理 や Cookieへの同意');
+		session.login = 'logined';
+	}
 	next();
 });
 
