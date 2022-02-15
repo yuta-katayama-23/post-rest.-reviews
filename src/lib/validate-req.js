8c075f48c6c22f1facde31b77cf4f62991532e76
Yuta Katayama <yutapilafu@gmail.com>
2022-02-15 13:47:27 +0900

    コメントアウトを戻す SameSite=Laxに設定

--- src/lib/validate-req.js
+++ src/lib/validate-req.js
@@ -4,19 +4,19 @@ import axios from 'axios';
 export default (option = {}) => {
 	return async (req, res, next) => {
 		const {
-			session
-			// body: { token },
-			// app: {
-			// 	locals: { tokens }
-			// }
+			session,
+			body: { token },
+			app: {
+				locals: { tokens }
+			}
 		} = req;
 
 		if (!session.id || !session.login)
 			next(new Error('You should be logged in.'));
 
-		// if (option.csrf && !tokens.verify(session.csrfSecret, token))
-		// 	next(new Error('Invalid Token.'));
-		// else delete session.csrfSecret;
+		if (option.csrf && !tokens.verify(session.csrfSecret, token))
+			next(new Error('Invalid Token.'));
+		else delete session.csrfSecret;
 
 		if (option.csrf) {
 			const params = new URLSearchParams();
